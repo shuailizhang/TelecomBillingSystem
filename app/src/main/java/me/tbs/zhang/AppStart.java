479b838290c1cb302a0d54d8d7e7eaf1dd33facc
dime <dime@danwi.me>
2016-01-05 00:47:17 +0800

    add start activity

---
+++ app/src/main/java/me/tbs/zhang/AppStart.java
@@ -0,0 +1,44 @@
+package me.tbs.zhang;
+
+import android.animation.Animator;
+import android.animation.ObjectAnimator;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * Created by hao on 2016/1/5.
+ */
+public class AppStart extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        View view = findViewById(R.id.start_root);
+        ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(view,"alpha", 1f, 0f).setDuration(500);
+        objectAnimator.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                startActivity(new Intent(AppStart.this, MainActivity.class));
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+    }
+}
