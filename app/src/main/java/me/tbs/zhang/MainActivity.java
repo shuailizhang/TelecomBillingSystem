948ed29b23af42ada2378b45591efb1a15c9e654
dime <dime@danwi.me>
2016-01-05 11:16:55 +0800

    主界面布局完善

--- app/src/main/java/me/tbs/zhang/MainActivity.java
+++ app/src/main/java/me/tbs/zhang/MainActivity.java
@@ -2,16 +2,59 @@ package me.tbs.zhang;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 
 public class MainActivity extends Activity {
+
+    Button findBtn;
+    EditText input_edit;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //初始化控件
+        findBtn = (Button) findViewById(R.id.main_btn);
+        input_edit = (EditText) findViewById(R.id.main_edit_text);
+        listView = (ListView) findViewById(R.id.main_listview);
+
+        findBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO:搜索获取数据
+            }
+        });
+    }
+
+    public class MyAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            return null;
+        }
     }
 
 }
